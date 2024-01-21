6a0a04431e0974cba5f85927d2fd8347f99c4cc3
Tuan-RMIT <tuan-rmit@192.168.1.6>
2024-01-21 11:09:13 +0700

    init project base

--- src/main/java/com/studio/climatechange/repository/CountryRepository.java
+++ src/main/java/com/studio/climatechange/repository/CountryRepository.java
@@ -9,7 +9,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 
 public interface CountryRepository extends JpaRepository<Country, Long>{
-
     @Query("SELECT c FROM country c WHERE c.someField = :someValue")
     Page<CountryDto> findBySomeField(@Param("someValue") String someValue, Pageable pageable);
+
+
 }
