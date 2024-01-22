0d86f949fc9c5940fa95d0408a692ce04a1ae37c
tuan-rmit <s4019549@rmit.edu.vn>
2024-01-22 22:13:44 +0700

    level3Controller

--- src/main/java/com/studio/climatechange/controller/Level3Controller.java
+++ src/main/java/com/studio/climatechange/controller/Level3Controller.java
@@ -2,7 +2,12 @@ package com.studio.climatechange.controller;
 
 import org.springframework.stereotype.Controller;
 
+import com.studio.climatechange.repository.CountryRepository;
+
 @Controller
 public class Level3Controller {
+    private CountryRepository countryRepository;
+
     
+
 }
